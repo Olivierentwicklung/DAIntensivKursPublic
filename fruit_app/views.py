4018c1b8455127e09b5d3c63b59ff9cd112c9de5
Olivierentwicklung <olivierentwicklung@gmail.com>
2026-04-14 13:12:15 +0200

    feat: update fruit list template to display ordered and unordered fruits separately

--- fruit_app/views.py
+++ fruit_app/views.py
@@ -3,70 +3,53 @@ from django.shortcuts import render
 
 
 def send_fruits(request: HttpRequest):
-    # fruits: list[dict[str, str | int]] = [
-    #     {"name": "Apfel", "gewicht": 150, "farbe": "Rot"},
-    #     {"name": "Banane", "gewicht": 120, "farbe": "Gelb"},
-    #     {"name": "Orange", "gewicht": 200, "farbe": "Orange"},
-    #     {"name": "Kirsche", "gewicht": 10, "farbe": "Rot"},
-    #     {"name": "Ananas", "gewicht": 1000, "farbe": "Gelb"},
-    # ]
     # Hier passiert der Fehler ohne safe=False
     return JsonResponse(fruits, safe=False)
 
 
 def send_fruits_template(request: HttpRequest | None):
-    # fruits: list[dict[str, str | int]] = [
-    #     {"name": "Apfel", "gewicht": 150, "farbe": "Rot"},
-    #     {"name": "Banane", "gewicht": 120, "farbe": "Gelb"},
-    #     {"name": "Orange", "gewicht": 200, "farbe": "Orange"},
-    #     {"name": "Kirsche", "gewicht": 10, "farbe": "Rot"},
-    #     {"name": "Ananas", "gewicht": 1000, "farbe": "Gelb"},
-    # ]
-    context: dict[str, list[dict[str, str | int]]] = {"fruits": fruits}
-    # Hier passiert der Fehler ohne safe=False
+    context: dict[str, list[dict[str, str | int | bool]]] = {"fruits": fruits}
     return render(request, "fruit_app/fruitlist.html", context)
 
 
 def fruit_info(request: HttpRequest | None):
-    # fruits: list[dict[str, str | int]] = [
-    #     {"name": "Apfel", "gewicht": 150, "farbe": "Rot"},
-    #     {"name": "Banane", "gewicht": 120, "farbe": "Gelb"},
-    #     {"name": "Orange", "gewicht": 200, "farbe": "Orange"},
-    #     {"name": "Kirsche", "gewicht": 10, "farbe": "Rot"},
-    #     {"name": "Ananas", "gewicht": 1000, "farbe": "Gelb"},
-    # ]
     return render(request, "fruit_app/info.html", fruits[0])
 
 
-fruits: list[dict[str, str | int]] = [
+fruits: list[dict[str, str | int | bool]] = [
     {
         "name": "Apfel",
         "gewicht": 150,
         "farbe": "Rot",
         "img": "../../static/fruit_app/img/apfel.webp",
+        "is_ordered": True,
     },
     {
         "name": "Banane",
         "gewicht": 120,
         "farbe": "Gelb",
         "img": "../../static/fruit_app/img/banane.jpg",
+        "is_ordered": False,
     },
     {
         "name": "Orange",
         "gewicht": 200,
         "farbe": "Orange",
         "img": "../../static/fruit_app/img/orange.jpg",
+        "is_ordered": True,
     },
     {
         "name": "Kirsche",
         "gewicht": 10,
         "farbe": "Rot",
         "img": "../../static/fruit_app/img/kirsche.jpg",
+        "is_ordered": False,
     },
     {
         "name": "Ananas",
         "gewicht": 1000,
         "farbe": "Gelb",
         "img": "../../static/fruit_app/img/ananas.jpg",
+        "is_ordered": True,
     },
 ]

99695c850dc78d7d9ae25a07b61b51363b42cb0f
Olivierentwicklung <olivierentwicklung@gmail.com>
2026-04-21 15:45:16 +0200

    feat: add Professor model with fields for first name, last name, and employee number

--- university_system_app/models.py
+++ university_system_app/models.py
@@ -24,3 +24,12 @@ class Studentenausweis(models.Model):
 
     def __str__(self):
         return f"Ausweis {self.card_number} - {self.issued_at} - {self.student}"
+
+
+class Professor(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    employee_number = models.CharField(max_length=18)
+
+    def __str__(self):
+        return f"Prof. {self.first_name} {self.last_name} ({self.employee_number})"

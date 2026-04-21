f5bc6d80ecdb538d8b614044b14e69cc5ecbed98
Olivierentwicklung <olivierentwicklung@gmail.com>
2026-04-21 15:38:55 +0200

    feat: add Student model with fields for first name, last name, and matriculation number

--- university_system_app/models.py
+++ university_system_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    matriculation_number = models.CharField(max_length=18)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.matriculation_number}"

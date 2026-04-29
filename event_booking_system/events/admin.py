2b613a7d142d5563cfa1a0c2d83c2a436aa39428
Olivierentwicklung <olivierentwicklung@gmail.com>
2026-04-29 13:02:50 +0200

    feat: enhance admin interface for events by customizing EventAdmin display

--- event_booking_system/events/admin.py
+++ event_booking_system/events/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 from .models import Event, EventCategory, Location
 
 # Register your models here.
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ["title", "category", "location", "date"]
+
+
 admin.site.register(EventCategory)
 admin.site.register(Location)
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
